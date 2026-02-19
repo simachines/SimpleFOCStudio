6f569eb4bfc63fb6b8bffe6cb650fe4ac7027c61
SiMachines <ernest18699@gmail.com>
2026-02-18 21:59:58 -0500

    Update dependencies and improve error handling in SimpleFOCStudio

--- simpleFOCStudio.py
+++ simpleFOCStudio.py
@@ -8,12 +8,15 @@ from PyQt5 import QtWidgets, QtCore
 from src.gui.mainWindow import UserInteractionMainWindow
 import sys
 import logging
+import os
+import traceback
 
 if __name__ == '__main__':
     try:
         QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True) 
         QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
-        logging.basicConfig(filename='.SimpleFOCConfigTool.log', filemode='w',
+        log_path = os.path.join(os.path.expanduser('~'), 'SimpleFOCStudio.log')
+        logging.basicConfig(filename=log_path, filemode='w',
                          format='%(name)s - %(levelname)s - %(message)s')
         app = QtWidgets.QApplication(sys.argv)
         mainWindow = QtWidgets.QMainWindow()
@@ -21,5 +24,14 @@ if __name__ == '__main__':
         userInteractionMainWindow.setupUi(mainWindow)
         mainWindow.show()
         sys.exit(app.exec_())
-    except Exception as exception:\
-    logging.error(exception, exc_info=True)
+    except Exception as exception:
+        logging.error(exception, exc_info=True)
+        try:
+            QtWidgets.QMessageBox.critical(
+                None,
+                'SimpleFOCStudio failed to start',
+                f'{exception}\n\nSee log: {os.path.join(os.path.expanduser("~"), "SimpleFOCStudio.log")}'
+            )
+        except Exception:
+            pass
+        traceback.print_exc()
